5a6e0d1308bc32bd31a3c0a61569120a73ec49b4
Subham raj <45128519+Shubhamr837@users.noreply.github.com>
2021-06-30 12:45:04 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-app.use('/', "Modi is the best prime minister of india");
+app.get('/', "Modi is the best prime minister of india");
 
 const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
 const PORT = process.env.PORT|| 5000;
