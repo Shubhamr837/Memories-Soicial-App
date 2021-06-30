0b92dd67b58f727cd14e8385a88e2f2c84fe220b
Subham raj <45128519+Shubhamr837@users.noreply.github.com>
2021-06-30 12:39:31 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,4 +17,4 @@ app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
 const PORT = process.env.PORT|| 5000;
 
-app.listen(3000)
+app.listen(3000);
